e42d87c3e57789bbdbd41f102548f78d45b3a6f7
ManuelDavidRamirez <Manuyagami95@gmail.com>
2023-05-15 17:15:46 -0300

    fixed FizzBuzz

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,12 +1,12 @@
 function fizz_buzz(n) {
     let result = [];
     for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
             result.push("Fizz");
         } else if (i % 5 === 0) {
             result.push("Buzz");
-        } else if (i % 3 === 0 && i % 5 === 0) {
-            result.push("FizzBuzz");
         } else {
             result.push(i);
         }
