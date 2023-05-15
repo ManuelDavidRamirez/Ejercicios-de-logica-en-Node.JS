9798f98eb4de71fc7bae8e1933664d0fee521bc8
ManuelDavidRamirez <Manuyagami95@gmail.com>
2023-05-15 17:06:26 -0300

    fizz_buzz done

---
+++ fizz_buzz.js
@@ -0,0 +1,20 @@
+function fizz_buzz(n) {
+    let result = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else {
+            result.push(i);
+        }
+    }
+    return result
+}
+
+let limit = 15;
+let cicle = fizz_buzz(limit);
+
+console.log(cicle);
